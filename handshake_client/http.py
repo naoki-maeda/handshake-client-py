959d361f876fc1ad8c38379dc1d4741e640b4f4c
naoki-maeda <infrontofden@gmail.com>
2020-04-07 17:14:49 +0900

    file rename and http timeout setting

--- handshake_client/http.py
+++ handshake_client/http.py
@@ -1,15 +1,19 @@
 import json
 import requests
 from typing import Union, List, Dict, Any
+from handshake_client.constant import TIMEOUT
 
 
 class Request:
-    def __init__(self, endpoint: str):
-        self.endpoint: str = endpoint
+    def __init__(self, endpoint: str, timeout: int = TIMEOUT):
+        assert type(endpoint) == str
+        assert type(timeout) == int
+        self.endpoint = endpoint
+        self.timeout = timeout
 
     def get(self, method: str) -> Union[List[Dict[str, Any]], Dict[str, Any]]:
         assert method == str
-        r = requests.get(self.endpoint + "/" + method)
+        r = requests.get(self.endpoint + "/" + method, timeout=self.timeout)
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
@@ -20,7 +24,7 @@ class Request:
         assert method == str
         assert params == dict
         headers = {"Content-Type": "application/json"}
-        r = requests.post(self.endpoint + "/" + method, data=json.dumps(params), headers=headers)
+        r = requests.post(self.endpoint + "/" + method, data=json.dumps(params), headers=headers, timeout=self.timeout)
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
